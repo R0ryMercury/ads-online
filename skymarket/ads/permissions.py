ef44277eca7c283b23a0a120bfbdd37c863cbdcb
rorymercury <12345hasek@gmail.com>
2023-01-04 23:38:31 +0300

    add permissions

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,27 @@
-# TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework import permissions
+
+
+class IsOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, "user"):
+            return (
+                request.user
+                and request.user.is_authenticated
+                and obj.user == request.user
+            )
+        return (
+            request.user and request.user.is_authenticated and obj.user == request.user
+        )
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_admin
+
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, "user"):
+            return request.user and request.user.is_admin
+        return request.user and request.user.is_admin
--- skymarket/ads/views.py
+++ skymarket/ads/views.py
@@ -1,4 +1,6 @@
 from rest_framework import pagination, viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from ads.permissions import IsOwner, IsAdmin
 from ads import serializers
 from ads.models import Ad
 
@@ -12,7 +14,7 @@ class AdViewSet(viewsets.ModelViewSet):
     pagination_class = AdPagination
 
     def get_serializer_class(self):
-        if self.action == "list":
+        if self.action in ["list", "me"]:
             return serializers.AdSerializer
         return serializers.AdDetailSerializer
 
@@ -20,7 +22,14 @@ class AdViewSet(viewsets.ModelViewSet):
         user = self.request.user
         serializer.save(author=user)
 
-    
+    def get_permissions(self):
+        permission_classes = (AllowAny,)
+        if self.action == "retrieve":
+            permission_classes = (AllowAny,)
+        elif self.action != "list":
+            permission_classes = IsOwner | IsAdmin
+        return tuple(permission() for permission in permission_classes)
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     pass
