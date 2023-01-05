8ad35ff9dbd61ba5446f8bf89fb1798ae140fc83
rorymercury <12345hasek@gmail.com>
2023-01-05 21:01:11 +0300

    change serializers fields

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -12,13 +12,22 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        exclude = ["ad", "author"]
+        fields = (
+            "pk",
+            "text",
+            "created_at",
+            "author_id",
+            "ad_id",
+            "author_first_name",
+            "author_last_name",
+            "author_image",
+        )
 
 
 class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
-        exclude = ["created_at", "author"]
+        fields = ("pk", "image", "title", "price", "description")
 
 
 class AdDetailSerializer(serializers.ModelSerializer):
@@ -28,4 +37,14 @@ class AdDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ad
-        exclude = ["author"]
+        fields = (
+            "pk",
+            "image",
+            "title",
+            "price",
+            "phone",
+            "author_first_name",
+            "author_last_name",
+            "description",
+            "author_id",
+        )
